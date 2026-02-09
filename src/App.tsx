30f10fd57165da32e44641e1dd0316b802b0e086
gmkoeb <gmkoeb@gmail.com>
2026-02-09 14:13:10 -0300

    lint and format with biomejs

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,26 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Root } from "./Root";
 import { CardGenerator } from "./pages/CardGenerator";
-import './index.css'
+import "./index.css";
 import { Home } from "./pages/Home";
 
 export function App() {
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <Root />,
-      children:[
-        { 
-          index: true,
-          path: "/",
-          Component: Home
-        },
-        {
-          path: 'card-generator',
-          Component: CardGenerator
-        }
-      ]
-    }
-  ])
- return <RouterProvider router={router} />
+	const router = createBrowserRouter([
+		{
+			path: "/",
+			element: <Root />,
+			children: [
+				{
+					index: true,
+					path: "/",
+					Component: Home,
+				},
+				{
+					path: "card-generator",
+					Component: CardGenerator,
+				},
+			],
+		},
+	]);
+	return <RouterProvider router={router} />;
 }
-
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,27 +1,38 @@
-import { useEffect, useState } from "react"
-import { charactersArray } from "../assets/characters"
+import { useEffect, useState } from "react";
+import { charactersArray } from "../assets/characters";
 
 export const Home = () => {
-  const [round, setRound] = useState(1)
-  const [characters, setCharacters] = useState(charactersArray)
+	const [round, setRound] = useState(1);
+	const [characters, setCharacters] = useState(charactersArray);
 
-  useEffect(() => {
-    setCharacters([...characters].sort((a, b) => a.name.localeCompare(b.name)))
-  }, [])
+	useEffect(() => {
+		setCharacters([...characters].sort((a, b) => a.name.localeCompare(b.name)));
+	}, [characters]);
 
-  return (
-    <div>
-      <h1 className="text-center text-6xl mb-4">Bingo da Liesel - {round}{"\u00AA"} Rodada</h1>
-      <div className="grid grid-cols-10 mx-20 mb-20">
-        {characters.map(character => {
-          return (
-            <div className='border w-full h-full flex flex-col items-center justify-center' key={character.name}>
-              <h1 className='my-2 text-3xl'>{character.name}</h1>
-              <img width={60} src={`images/${character.name}.webp`} className='mb-2' alt="" />
-            </div>
-          )
-        })}
-      </div>
-    </div>
-  )
-}
+	return (
+		<div>
+			<h1 className="text-center text-6xl mb-4">
+				Bingo da Liesel - {round}
+				{"\u00AA"} Rodada
+			</h1>
+			<div className="grid grid-cols-10 mx-20 mb-20">
+				{characters.map((character) => {
+					return (
+						<div
+							className="border w-full h-full flex flex-col items-center justify-center"
+							key={character.name}
+						>
+							<h1 className="my-2 text-3xl">{character.name}</h1>
+							<img
+								width={60}
+								src={`images/${character.name}.webp`}
+								className="mb-2"
+								alt=""
+							/>
+						</div>
+					);
+				})}
+			</div>
+		</div>
+	);
+};
