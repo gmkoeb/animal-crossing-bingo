ffbcc1a613c541ebe0d7887bcc894bc0185626c5
gmkoeb <gmkoeb@gmail.com>
2026-02-09 10:17:47 -0300

    add dynamic bingo card generation

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ interface Character {
 }
 
 function App() {
-  const [characters, setCharacters] = useState<Character[]>([])
+  const [cards, setCards] = useState<Character[][]>([[]])
 
   function shuffleArray(array: Character[]) {
     for (let i = array.length - 1; i > 0; i--) {
@@ -17,119 +17,55 @@ function App() {
     }
     return array;
   }
+  
+  function generateBingoCard(array: Character[], quantity: number){
+    setCards([])
+    for (let i = 0; i < quantity; i++){
+      const bingoArray = shuffleArray(array).slice(0, 25)
+      bingoArray[12] = {"name": "Free", "category": "Free"}
+      setCards(prev => [
+        ...prev,
+        bingoArray
+      ])
+    }
+  }
 
   useEffect(() => {
-    const bingoArray = shuffleArray(charactersArray).slice(0, 25)
-    bingoArray[12] = {"name": "Free", "category": "Free"}
-    setCharacters(bingoArray)
+    generateBingoCard(charactersArray, 25)
   }, [])
 
   return (
     <div className='grid grid-cols-4'>
-      <div className='grid grid-cols-5 mx-10 my-20'>
-        <div className='flex text-6xl absolute gap-11.5 ml-6 -translate-y-20'>
-          <p>B</p>
-          <p>I</p>
-          <p>N</p>
-          <p>G</p>
-          <p>O</p>
+      {cards.map(card => {
+        return (
+          <div className='grid grid-cols-5 mx-10 my-20'>
+            <div className='flex text-6xl absolute gap-11.5 ml-6 -translate-y-20'>
+              <p>B</p>
+              <p>I</p>
+              <p>N</p>
+              <p>G</p>
+              <p>O</p>
+          </div>
+          {card.map(character => {
+            return (
+              <div className='border items-center justify-center text-center flex flex-col' key={character.name}>
+                {character.name === "Free" ? 
+                  <div className='w-full'>
+                    <h1 className='text-center text-2xl font-bold text-green-400'>{character.name}</h1>
+                    <img width={60} src={`images/${character.name}.webp`} className='mb-2' alt="" />
+                  </div>
+                  :
+                  <>
+                    <h1 className='text-center my-2'>{character.name}</h1>
+                    <img width={30} src={`images/${character.name}.webp`} className='mb-2' alt="" />
+                  </>
+                }
+              </div>
+            )
+          })}
         </div>
-        {characters.map(character => {
-          return (
-            <div className='border items-center justify-center text-center flex flex-col' key={character.name}>
-              {character.name === "Free" ? 
-                <div className='w-full'>
-                  <h1 className='text-center text-2xl font-bold text-green-400'>{character.name}</h1>
-                  <img width={60} src={`images/${character.name}.webp`} className='mb-2' alt="" />
-                </div>
-                :
-                <>
-                  <h1 className='text-center my-2'>{character.name}</h1>
-                  <img width={30} src={`images/${character.name}.webp`} className='mb-2' alt="" />
-                </>
-              }
-            </div>
-          )
-        })}
-      </div>
-      <div className='grid grid-cols-5 mx-10 my-20'>
-        <div className='flex text-6xl absolute gap-11.5 ml-6 -translate-y-20'>
-          <p>B</p>
-          <p>I</p>
-          <p>N</p>
-          <p>G</p>
-          <p>O</p>
-        </div>
-        {characters.map(character => {
-          return (
-            <div className='border items-center justify-center text-center flex flex-col' key={character.name}>
-              {character.name === "Free" ? 
-                <div className='w-full'>
-                  <h1 className='text-center text-2xl font-bold text-green-400'>{character.name}</h1>
-                  <img width={60} src={`images/${character.name}.webp`} className='mb-2' alt="" />
-                </div>
-                :
-                <>
-                  <h1 className='text-center my-2'>{character.name}</h1>
-                  <img width={30} src={`images/${character.name}.webp`} className='mb-2' alt="" />
-                </>
-              }
-            </div>
-          )
-        })}
-      </div>
-      <div className='grid grid-cols-5 mx-10 my-20'>
-        <div className='flex text-6xl absolute gap-11.5 ml-6 -translate-y-20'>
-          <p>B</p>
-          <p>I</p>
-          <p>N</p>
-          <p>G</p>
-          <p>O</p>
-        </div>
-        {characters.map(character => {
-          return (
-            <div className='border items-center justify-center text-center flex flex-col' key={character.name}>
-              {character.name === "Free" ? 
-                <div className='w-full'>
-                  <h1 className='text-center text-2xl font-bold text-green-400'>{character.name}</h1>
-                  <img width={60} src={`images/${character.name}.webp`} className='mb-2' alt="" />
-                </div>
-                :
-                <>
-                  <h1 className='text-center my-2'>{character.name}</h1>
-                  <img width={30} src={`images/${character.name}.webp`} className='mb-2' alt="" />
-                </>
-              }
-            </div>
-          )
-        })}
-      </div>
-      <div className='grid grid-cols-5 mx-10 my-20'>
-        <div className='flex text-6xl absolute gap-11.5 ml-6 -translate-y-20'>
-          <p>B</p>
-          <p>I</p>
-          <p>N</p>
-          <p>G</p>
-          <p>O</p>
-        </div>
-        {characters.map(character => {
-          return (
-            <div className='border items-center justify-center text-center flex flex-col' key={character.name}>
-              {character.name === "Free" ? 
-                <div className='w-full'>
-                  <h1 className='text-center text-2xl font-bold text-green-400'>{character.name}</h1>
-                  <img width={60} src={`images/${character.name}.webp`} className='mb-2' alt="" />
-                </div>
-                :
-                <>
-                  <h1 className='text-center my-2'>{character.name}</h1>
-                  <img width={30} src={`images/${character.name}.webp`} className='mb-2' alt="" />
-                </>
-              }
-            </div>
-          )
-        })}
-      </div>
+        )
+      })}
     </div>
   )
 }
