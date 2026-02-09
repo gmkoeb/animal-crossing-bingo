27241a0a76da670cbcd3d208ed9adb4550041c29
gmkoeb <gmkoeb@gmail.com>
2026-02-09 12:57:25 -0300

    implement card hashing to guarantee each card is unique
    Now a card is included in the cardsArray if its hash is different
    from any hash inside hashes set.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import { useEffect, useState } from 'react'
 import './index.css'
 import { charactersArray } from './assets/characters'
-import { positions } from './assets/positions'
-
-type BingoCategory = "B" | "I" | "N" | "G" | "O"
+import { positions, type BingoCategory } from './assets/positions'
 
 export interface Character {
   name: string
   category: BingoCategory
 }
 
-function App() {
-  const [cards, setCards] = useState<Character[][]>([[]])
+interface Card {
+  characters: Character[]
+  hash: string
+}
 
+function App() {
+  const [cards, setCards] = useState<Card[]>([])
   function shuffleArray(array: Character[]) {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
@@ -22,41 +24,67 @@ function App() {
   }
 
   function generateCardByCategory() {
-    const categories: BingoCategory[] = ["B", "I", "N", "G", "O"]
-    const card: Character[] = []
-    categories.forEach(category => {
+    const card: Card = {
+      characters: [],
+      hash: ""
+    }
+    Object.keys(positions).forEach(category => {
       const filteredCharacters = charactersArray.filter(filteredCharacter => {
         return filteredCharacter.category === category
       })
-      positions[category].forEach(position => {
+      positions[category as BingoCategory].forEach(position => {
         const randomCharacter = shuffleArray(filteredCharacters).pop()
         if (randomCharacter){
-          card[position] = randomCharacter
+          card.characters[position] = randomCharacter
         }
       })
     })
     return card
   }
-  
-  function generateBingoCards(quantity: number){
-    const cardsArray: Character[][] = []
-    for (let i = 0; i < quantity; i++){
-      const card = generateCardByCategory()
-      card[12] = {"name": "Free", "category": "N"}
-      cardsArray.push(card)
+
+  function simpleHash(str: string): number {
+    let hash = 0
+    for (let i = 0; i < str.length; i++) {
+      hash = (hash << 5) - hash + str.charCodeAt(i)
+      hash |= 0
     }
-    setCards(cardsArray) 
+    return hash
   }
 
+  function generateCardHash(card: Card) {
+    let i = 0;
+    let hash = ""
+    card.characters.forEach(character => {
+      hash += simpleHash(character.name + i)
+      i++
+    })
+    return hash
+  }
+
+    function generateBingoCards(quantity: number){
+      const cardsArray: Card[] = []
+      const hashes: Set<string> = new Set()
+      while (cardsArray.length < quantity){
+        const card = generateCardByCategory()
+        card.characters[12] = {"name": "Free", "category": "N"}
+        card.hash = generateCardHash(card)
+        if (!hashes.has(card.hash)){
+          hashes.add(card.hash)
+          cardsArray.push(card)
+        }
+      }
+      setCards(cardsArray) 
+    }
+
   useEffect(() => {
-    generateBingoCards(10)
+    generateBingoCards(25)
   }, [])
 
   return (
     <div className='grid grid-cols-4'>
       {cards.map(card => {
         return (
-          <div className='grid grid-cols-5 mx-10 my-20'>
+          <div key={`${card.hash}`} className='grid grid-cols-5 mx-10 my-20'>
             <div className='flex text-6xl absolute gap-11.5 ml-6 -translate-y-20'>
               <p>B</p>
               <p>I</p>
@@ -64,7 +92,7 @@ function App() {
               <p>G</p>
               <p>O</p>
           </div>
-          {card.map(character => {
+          {card.characters.map(character => {
             return (
               <div className='border items-center justify-center text-center flex flex-col' key={character.name}>
                 {character.name === "Free" ? 
--- src/assets/positions.ts
+++ src/assets/positions.ts
@@ -1,7 +1,9 @@
-export const positions = {
-  "B": [0, 5, 10, 15, 20], 
-  "I": [1, 6, 11, 16, 21],
-  "N": [2, 7, 12, 17, 22],
-  "G": [3, 8, 13, 18, 23],
-  "O": [4, 9, 14, 19, 24]
+export type BingoCategory = "B" | "I" | "N" | "G" | "O"
+
+export const positions: Record<BingoCategory, number[]> = {
+  B: [0, 5, 10, 15, 20], 
+  I: [1, 6, 11, 16, 21],
+  N: [2, 7, 12, 17, 22],
+  G: [3, 8, 13, 18, 23],
+  O: [4, 9, 14, 19, 24]
 }
